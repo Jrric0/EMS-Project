19396522db62bcf402715893fc7fca25e2ffa587
Jrric0 <128221793+Jrric0@users.noreply.github.com>
2023-03-18 18:13:44 +0800

    Add files via upload

---
+++ DashBoard/index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta charset="UTF-8">
+  <title>Dashboard</title>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+    <div class="d-flex" id="wrapper">
+        <!-- Sidebar -->
+        <div class="bg-white" id="sidebar-wrapper">
+            <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i
+                    class="fas fa-user-secret me-2"></i>EMS</div>
+            <div class="list-group list-group-flush my-3">
+                <a href="#" class="list-group-item list-group-item-action bg-transparent second-text active"><i
+                        class="fas fa-tachometer-alt me-2"></i>Dashboard 1</a>
+                <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                        class="fas fa-project-diagram me-2"></i>Dashboard 2</a>
+            </div>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
+                <div class="d-flex align-items-center">
+                    <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
+                    <h2 class="fs-2 m-0">Dashboard</h2>
+                </div>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </nav>
+            <!-- Container for Rows -->
+            <div class="container-fluid px-5">
+                <!-- Row 1 -->
+                <div class="row g-4 my-2">
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-4 text-center">Line 1 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg  justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-4 text-center">Line 2 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>
+                        
+                        </div>
+                    </div>
+
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                 <h3 class="fs-4 text-center">Line 3 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div> 
+                        </div>
+                    </div>
+
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                               <h3 class="fs-4 text-center">Line 4 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>
+                        </div>
+                    </div>
+                     <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-4 text-center">Line 5 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                             <h3 class="fs-4 text-center">Line 6 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>              
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                 <h3 class="fs-4 text-center">Line 7 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>          
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                 <h3 class="fs-4 text-center">Line 8 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>          
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                 <h3 class="fs-4 text-center">Line 9 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>          
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                 <h3 class="fs-4 text-center">Line 10 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>          
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                 <h3 class="fs-4 text-center">Line 11 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>          
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                 <h3 class="fs-4 text-center">Line 12 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>          
+                        </div>
+                    </div>
+
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                 <h3 class="fs-4 text-center">Line 13 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>          
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                 <h3 class="fs-4 text-center">Line 14 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>          
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                 <h3 class="fs-4 text-center">Line 15 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>          
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                 <h3 class="fs-4 text-center">Line 16 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>          
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                 <h3 class="fs-4 text-center">Line 17 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>          
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="p-3 bg-white shadow-lg justify-content-around align-items-center rounded">
+                            <div>
+                                 <h3 class="fs-4 text-center">Line 18 </h3>
+                                <p class="fs-5">Model: </p>
+                                 <p class="fs-5">Target: </p>
+                                  <p class="fs-5">Actual: </p>
+                                   <p class="fs-5">Status: </p>
+                            </div>          
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script>
+        var el = document.getElementById("wrapper");
+        var toggleButton = document.getElementById("menu-toggle");
+
+        toggleButton.onclick = function () {
+            el.classList.toggle("toggled");
+        };
+    </script>
+</body>
+
+</html>
